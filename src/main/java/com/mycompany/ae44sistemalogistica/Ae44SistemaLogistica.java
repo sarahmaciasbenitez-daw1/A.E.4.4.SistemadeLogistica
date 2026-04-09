673c7be7a80753741c04896931efbbf34a8e691c
Sarah Macias Benitez <sarahmaciasbenitez@ieselrincon.es>
2026-04-09 10:05:35 +0100

    Entrega Final 4.4 Sistema de Logística

--- src/main/java/com/mycompany/ae44sistemalogistica/Ae44SistemaLogistica.java
+++ src/main/java/com/mycompany/ae44sistemalogistica/Ae44SistemaLogistica.java
@@ -7,21 +7,34 @@ package com.mycompany.ae44sistemalogistica;
  *
  * @author sarah
  */
-public class Main {
+public class Ae44SistemaLogistica {
 
+    /**
+     *
+     * @param args
+     */
     public static void main(String[] args) {
 
         Almacen almacen = new Almacen();
 
-        Paquete p1 = new Paquete("A1", 10, "Madrid");
-        Paquete p2 = new Paquete("B2", 25, "Barcelona");
-        Paquete p3 = new Paquete("C3", 5, "Valencia");
-        Paquete p4 = new Paquete("A1", 15, "Sevilla");
+        System.out.println("--- Recepción ---");
+        Paquete p1 = new Paquete("A1", 10.0, "Madrid");
+        Paquete p2 = new Paquete("B2", 25.0, "Barcelona");
+        Paquete p3 = new Paquete("C3", 5.0, "Valencia");
+        Paquete p4 = new Paquete("A1", 15.0, "Sevilla");
 
         almacen.recepcionarPaquete(p1, 0, 0);
         almacen.recepcionarPaquete(p2, 0, 1);
         almacen.recepcionarPaquete(p3, 1, 0);
         almacen.recepcionarPaquete(p4, 1, 1);
 
+        System.out.println("\n--- cola de salida ---");
+        almacen.enviarACamion(0, 0);
+        almacen.enviarACamion(0, 1);
+
+        System.out.println("\n--- mantenimiento de seguridad (máx 20kg) ---");
+        almacen.mantenimientoSeguridad(20.0);
+
+        almacen.mostrarEstadisticas();
     }
 }
